88f4d73a4662e163881c2c407b42e61ecdcb343b
Andrew Goacher <goach@hey.com>
2020-12-02 21:24:34 +0000

    Output second challenge for day 1

--- src/adventofcode/Program.cs
+++ src/adventofcode/Program.cs
@@ -5,5 +5,8 @@ using adventofcode;
 
 var day_1_input = File.ReadAllLines("./content/day_1_input.txt").Select(x => Convert.ToInt32(x));
 var day_1_part_1_output = SumNumbers.GetMultipleOfTwoNumbers(day_1_input, 2020);
+var day_1_part_2_output = SumNumbers.GetMultipleOfThreeNumbers(day_1_input, 2020);
 
-Console.WriteLine($"day 1: {day_1_part_1_output}");
+Console.WriteLine("day 1:");
+Console.WriteLine($"\t\t {day_1_part_1_output}");
+Console.WriteLine($"\t\t {day_1_part_2_output}");
