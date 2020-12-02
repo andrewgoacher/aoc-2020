30c378ca3a0921f8184d160997fdf531eb64b133
Andrew Goacher <goach@hey.com>
2020-12-02 22:01:08 +0000

    Results for day 2 part 1

--- src/adventofcode/Program.cs
+++ src/adventofcode/Program.cs
@@ -7,6 +7,15 @@ var day_1_input = File.ReadAllLines("./content/day_1_input.txt").Select(x => Con
 var day_1_part_1_output = SumNumbers.GetMultipleOfTwoNumbers(day_1_input, 2020);
 var day_1_part_2_output = SumNumbers.GetMultipleOfThreeNumbers(day_1_input, 2020);
 
+var day_2_input = File.ReadAllLines("./content/day_2_input.txt")
+    .Select(x => new PasswordToken(x));
+
+var day_2_part_1_output = day_2_input
+    .Count(x => x.IsValid());
+
 Console.WriteLine("day 1:");
 Console.WriteLine($"\t\t {day_1_part_1_output}");
 Console.WriteLine($"\t\t {day_1_part_2_output}");
+
+Console.WriteLine("day 2:");
+Console.WriteLine($"\t\t {day_2_part_1_output}");
