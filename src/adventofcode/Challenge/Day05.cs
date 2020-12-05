7aa9d1eff9231b16a08b2cf71df042a8356b9608
Andrew Goacher <goach@hey.com>
2020-12-05 12:35:47 +0000

    Complete day 5

--- src/adventofcode/Challenge/Day05.cs
+++ src/adventofcode/Challenge/Day05.cs
@@ -11,14 +11,35 @@ namespace adventofcode.Challenge
         {
             var day_5_input = File.ReadAllLines("./content/day_5_input.txt");
 
-            var day_5_part_1_output = day_5_input
+            var seatIds = day_5_input
                 .Select(x => new BoardingPass(x).SeatId)
                 .OrderByDescending(x => x)
+                .ToList();
+
+            var day_5_part_1_output =
+                seatIds
                 .First();
-            
+
+            var range = Enumerable.Range(0, day_5_part_1_output)
+                .Select((_, index) => index);
+
+            var intersect = range.Except(seatIds);
+
+            var day_5_part_2_output = -1;
+            foreach (var seat in intersect)
+            {
+                var seats = seatIds.Where(x => x == seat + 1 || x == seat - 1);
+                if (seats.Count() == 2)
+                {
+                    day_5_part_2_output = seat;
+                    break;
+
+                }
+            }
+
             Console.WriteLine("day 5:");
             Console.WriteLine($"\t\t {day_5_part_1_output}");
-            // Console.WriteLine($"\t\t {day_1_part_2_output}");
+            Console.WriteLine($"\t\t {day_5_part_2_output}");
         }
     }
 }
