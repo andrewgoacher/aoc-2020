37320ca64a54bb13c92fb5473ad84ac6756ee15c
Andrew Goacher <goach@hey.com>
2020-12-12 17:23:29 +0000

    Add input and program shell

---
+++ src/Day12/Program.cs
@@ -0,0 +1,10 @@
+using System.IO;
+using System.Linq;
+using static Common.Output;
+
+// var lines = File.ReadAllLines("./input.txt").ToArray();
+
+var part1 = 0;
+var part2 = 0;
+
+WriteChallenge(12, part1, part2);
